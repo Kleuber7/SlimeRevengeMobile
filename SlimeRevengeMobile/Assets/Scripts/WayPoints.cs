05bfe4ab24d5bcc2591433161e631b1a1b62767e
iansieiro <ian.sieiro@hotmail.com>
2022-04-11 22:56:28 -0300

    Att

--- SlimeRevengeMobile/Assets/Scripts/WayPoints.cs
+++ SlimeRevengeMobile/Assets/Scripts/WayPoints.cs
@@ -7,5 +7,6 @@ public class WayPoints : MonoBehaviour
     public Transform[] points1;
     public Transform[] points2;
     public Transform[] points3;
-
+    public Transform[] points4;
+    public Transform[] points5;
 }
